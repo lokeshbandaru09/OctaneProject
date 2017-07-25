ea739dc9d653a2549516645e222a4b8988cbf6ef
Lokesh Bandaru <lokesh.bandaru@lemontree.se>
2017-07-25 21:47:17 +0200

    Ignore File is added again

--- src/main/java/com/LT/Main.java
+++ src/main/java/com/LT/Main.java
@@ -1,11 +1,13 @@
 package com.LT;
 
+import org.junit.Test;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
 
         System.out.println("Hello World");
-        
+        System.out.println("Hello World 1");
     }
 }
