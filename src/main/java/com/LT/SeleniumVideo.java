dfe9c0c2b2ffec15d232b897e8d4109c126b576e
lokesh.bandaru@lemontree.se <tLxMJKet09>
2017-08-17 15:49:33 +0200

    4th

--- src/main/java/com/LT/SeleniumVideo.java
+++ src/main/java/com/LT/SeleniumVideo.java
@@ -18,14 +18,17 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.ie.*;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
 
+import static java.lang.Thread.sleep;
+
 public class SeleniumVideo{
     @Test
-public void JenkinsTest ()
-    {
+public void JenkinsTest () throws InterruptedException {
      // Code to make run faster in Internet Explorer
         DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
         capabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
@@ -34,13 +37,18 @@ public void JenkinsTest ()
      //   System.setProperty("webdriver.ie.driver", "C:\\Program Files\\Internet Explorer\\iexplore.exe" );
       System.setProperty("webdriver.chrome.driver", "C:\\Users\\Administrator\\Downloads\\chromedriver_win32\\chromedriver.exe" );
 
-        WebDriver driver = new ChromeDriver();
+        //WebDriver driver = new ChromeDriver();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--start-maximized");
+        WebDriver driver = new ChromeDriver(chromeOptions);
+
 
       //  WebDriver driver = new InternetExplorerDriver(capabilities);
         System.out.println("Hi, Welcome to Devops Demo");
 
     //   Code for Amazon wesite to serach for desired Name : DEVOPS BOOKS
         driver.navigate().to("https://www.amazon.com");
+       // ((IJavaScriptExecutor)driver).ExecuteScript("window.resizeTo(1024, 768);");
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
         driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Lemontree AB");
@@ -63,11 +71,17 @@ public void JenkinsTest ()
 
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.findElement(By.id("menu-item-1859")).click();
-        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 
-       /* driver.findElement(By.id("menu-item-2120")).click();
-        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 
+
+        driver.findElement(By.id("menu-item-2120")).click();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("/html/body[@class='page-template page-template-page-templates page-template-page-contact page-template-page-templatespage-contact-php page page-id-889 single-author']/div[@id='page']/div[@id='main']/div[@id='primary contact-page']/div[@id='content']/div[@class='blocks-container content-container']/div[@class='inner']/article[@id='post-889']/div[@class='entry-content post-separator']/div[@id='wpcf7-f4755-p889-o1']/form[@class='wpcf7-form']/div[@id='contact-form']/fieldset/p[1]/span[@class='wpcf7-form-control-wrap your-name']/input[@class='wpcf7-form-control wpcf7-text wpcf7-validates-as-required']")).sendKeys("Your Name");
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("/html/body[@class='page-template page-template-page-templates page-template-page-contact page-template-page-templatespage-contact-php page page-id-889 single-author']/div[@id='page']/div[@id='main']/div[@id='primary contact-page']/div[@id='content']/div[@class='blocks-container content-container']/div[@class='inner']/article[@id='post-889']/div[@class='entry-content post-separator']/div[@id='wpcf7-f4755-p889-o1']/form[@class='wpcf7-form']/div[@id='contact-form']/fieldset/p[2]/span[@class='wpcf7-form-control-wrap your-email']/input[@class='wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email']")).sendKeys("Your Email");
+
+  /*
         driver.findElement(By.name("your-name")).sendKeys("Your Name");
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
         driver.findElement(By.name("your-email")).sendKeys("Your Email");
