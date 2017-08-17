c092c79d5ccf68c9a688f9dce1269f34197a84b5
lokesh.bandaru@lemontree.se <tLxMJKet09>
2017-08-17 11:44:07 +0200

    Test to see the Chaining fo the jobs

---
+++ src/test/java/Runner/TestRunner.kt
@@ -0,0 +1,13 @@
+package Runner
+
+
+import cucumber.api.junit.Cucumber
+import cucumber.api.testng.AbstractTestNGCucumberTests
+import org.junit.runner.RunWith
+import cucumber.api.CucumberOptions
+
+
+//@RunWith(Cucumber.class)
+@CucumberOptions(features = arrayOf("src/test/java/features"), format = arrayOf("json:target/cucumber.json", "html:target/site/cucumber-pretty"), glue = arrayOf("Steps"))
+//public class TestRunner{
+class TestRunner : AbstractTestNGCucumberTests()
